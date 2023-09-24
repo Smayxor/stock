161cfa338dfb946a5e8a79d9c0074106038d90df
Smayxor <118859839+Smayxor@users.noreply.github.com>
2023-09-24 07:47:48 -0700

    Add files via upload

--- spr.py
+++ spr.py
@@ -18,7 +18,7 @@ def tick():
 		angle = (angle + 5.0) % 360
 		colorIndex = (colorIndex + 1) % 7
 		x1, y1 = -50.0, -50.0
-		x2, y2 = 0.0, -75.0
+		x2, y2 = 0.0, -100.0
 		"""
 		xAngle = 0.0
 		yAngle = 0.0
@@ -37,14 +37,15 @@ def tick():
 		xAngle = xAngle / 90.0   #make angles a % of movement along each axis
 		yAngle = yAngle / 90.0
 		"""  #The Above if statements have been shrunk to the equation below
-		flippy = ((angle // 180) * -2) + 1  #		flippy = -1 if angle // 180 else 1
+		flippy = ((angle // 180) * -2) + 1
 		xAngle = (abs((((angle // 90) % 2) * 90) - (angle % 90)) * flippy) / 90
-		yAngle = (((angle - 90) - ((angle // 180) * 180)) * flippy) / 90
-		
+		yAngle = -(abs(180 - angle) - 90) / 90
+
 		def drawDot(x, y):
 			newX = 150 + (x*xAngle) - (y*yAngle)
 			newy = 150 + (x*yAngle) + (y*xAngle)
 			canvas.create_line(newX,newy,newX + 1,newy + 1, fill=COLORS[colorIndex], width=3)
+		#canvas.delete("all")
 		drawDot(x1, y1)
 		drawDot(x2, y2)
 		win.update()

a846ee26c7c02df6fee714cae6d1ac1af8e7f2cf
Smayxor <118859839+Smayxor@users.noreply.github.com>
2023-11-21 06:54:43 -0800

    Add files via upload

--- datapuller.py
+++ datapuller.py
@@ -51,55 +51,6 @@ def getMultipleDTEOptionChain(ticker, days):
 	return days
 
 #{'symbol': 'SPY231030C00499000', 'description': 'SPY Oct 30 2023 $499.00 Call', 'exch': 'Z', 'type': 'option', 'last': 0.01, 'change': 0.0, 'volume': 0, 'open': None, 'high': None, 'low': None, 'close': None, 'bid': 0.0, 'ask': 0.01, 'underlying': 'SPY', 'strike': 499.0, 'greeks': {'delta': 0.0, 'gamma': 0.0, 'theta': 0.0, 'vega': 2e-05, 'rho': 0.0, 'phi': 0.0, 'bid_iv': 0.0, 'mid_iv': 0.716638, 'ask_iv': 0.716638, 'smv_vol': 0.16, 'updated_at': '2023-10-27 20:00:01'}, 'change_percentage': 0.0, 'average_volume': 0, 'last_volume': 11, 'trade_date': 1697812231388, 'prevclose': 0.01, 'week_52_high': 0.0, 'week_52_low': 0.0, 'bidsize': 0, 'bidexch': 'Q', 'bid_date': 1698437676000, 'asksize': 5608, 'askexch': 'X', 'ask_date': 1698437691000, 'open_interest': 36, 'contract_size': 100, 'expiration_date': '2023-10-30', 'expiration_type': 'weeklys', 'option_type': 'call', 'root_symbol': 'SPY'}
-"""def getOldGEX(options):  #Original list of tuples code
-	index = 0
-	strikes = []
-	def findIndex( strike ): #loop from end of list, confirm we are using correct index
-		index = len(strikes) - 1
-		while strikes[index][0] != strike: index -= 1
-		return index
-	
-	for option in options:
-		strike = option['strike'] 
-		call = 1 if option['option_type'] == 'call' else -1
-		gamma = 0 
-		iv = 0
-		if option['greeks'] is not None:
-			gamma = option['greeks']['gamma']
-			iv = option['greeks']['mid_iv']
-		oi = option['open_interest']
-		volume = option['volume']
-		gex = oi * gamma * call
-		#exDate = option['expiration_date']
-		bid = option['bid']
-		ask = option['ask']
-		bidSize = option['bidsize']
-		askSize = option['asksize']
-		#if strike == 4350 : print( option )
-		if (len(strikes) == 0) or (strikes[index][0] != strike): #fast, assumes strikes are in order
-			strikes.append( (strike, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) ) #0-Strike, 1-CallGEX, 2-CallOI,  3-PutGEX, 4-PutOI, 5-IV, 6-CallBid, 7-CallAsk, 8-PutBid, 9-PutAsk, 10-CallVolume, 11-CallBidSize, 12-CallAskSize, 13-PutVolume, 14-PutBidSize, 15-PutAskSize
-			index = findIndex(strike) # always make sure we're on the right strike index
-		#combine tuples for that specific strike.  Combine Calls + Puts + DifferentRootSymbol - SPX/SPXW
-		
-		
-		if call == 1: strikes[index] = (strike, strikes[index][1] + gex, strikes[index][2] + oi, strikes[index][3], strikes[index][4], iv, bid, ask, strikes[index][8], strikes[index][9], volume, bidSize, askSize, strikes[index][13], strikes[index][14], strikes[index][15])
-		else: strikes[index] = (strike, strikes[index][1], strikes[index][2], strikes[index][3] + gex, strikes[index][4] + oi, iv, strikes[index][6], strikes[index][7], bid, ask, strikes[index][10], strikes[index][11], strikes[index][12], volume, bidSize, askSize)
-
-		
-	for index in range( len(strikes) ):
-		strike = strikes[index]
-		totalOI = strike[2] + strike[4]
-		totalGEX = strike[1] + strike[3]
-		strike = (strike[0], totalGEX, totalOI, strike[1], strike[2], strike[3], strike[4], strike[5], strike[6], strike[7], strike[8], strike[9], strike[10], strike[11], strike[12], strike[13], strike[14], strike[15] )#0-Strike, 1-TotalGEX, 2-TotalOI, 3-CallGEX, 4-CallOI,  5-PutGEX, 6-PutOI, 7-IV, 8-CallBid, 9-CallAsk, 10-PutBid, 11-PutAsk, 12-CallVolume, 13-CallBidSize, 14-CallAskSize, 15-PutVolume, 16-PutBidSize, 17-PutAskSize
-		for i in range( 17 ):  #filter out any suprises
-			if strike[i] == None:
-				tmp = list( strike )
-				tmp[i] = 0
-				strike = tuple(tmp)
-
-		strikes[index] = strike
-	return strikes
-"""
 def getGEX(options, chartType = 0):  #New test code
 	index = 0
 	strikes = []
@@ -143,20 +94,12 @@ def getGEX(options, chartType = 0):  #New test code
 		#strikes[index] = tmp
 
 	for index in range( len(strikes) ):
-		tmp = strikes[index]
-		tmp[GEX_TOTAL_GEX] = tmp[GEX_CALL_GEX] + tmp[GEX_PUT_GEX]
-		tmp[GEX_TOTAL_OI] = tmp[GEX_CALL_OI] + tmp[GEX_PUT_OI]
-
-		
-		for i in range( 17 ): 
-			if tmp[i] == None: tmp[i] = 0
-		#tmp = [0 if i is None else i for i in tmp]#filter out any suprises
-
-
-		for i in tmp:
-			if i == None: print(f'Failed at {tmp}')
-
-		
+		strikes[index][GEX_TOTAL_GEX] = strikes[index][GEX_CALL_GEX] + strikes[index][GEX_PUT_GEX]
+		strikes[index][GEX_TOTAL_OI] = strikes[index][GEX_CALL_OI] + strikes[index][GEX_PUT_OI]
+		#for i in range( 17 ): 		#	if tmp[i] == None: tmp[i] = 0
+		strikes[index] = [0 if i is None else i for i in strikes[index]]#filter out any suprises
+#		for i in strikes[index]:
+#			if i == None: print(f'Failed at {strikes[index]}')		
 	return strikes
 
 def calcZeroGEX(data): #def add(a, b): return (b[0], a[1] + b[1]) #cumsum = list(accumulate(data, add)) #return min(cumsum, key=lambda i: i[1])[0]
@@ -325,4 +268,20 @@ def modifyOrder(orderID, type, duration, price, stop):
 def placeOptionOrder(symbol, price, stop, ticker = 'SPY', side='buy_to_open', quantity='1', type='limit', duration='day', tag='test'):
 	param = {'class': 'option', 'symbol': ticker, 'option_symbol': symbol, 'side': side, 'quantity': quantity, 'type': type, 'duration': duration, 'price': price, 'stop': stop, 'tag': tag}
 	return requests.post(f'https://api.tradier.com/v1/accounts/{TRADIER_ACCOUNT_ID}/orders', data=param, headers=TRADIER_HEADER).json()
-	
+
+
+
+"""testList = [[4655.0, 0, 0, 0, 0, 0, 0, 0, None, None, None, None, 0, 0, 0, 0, 0, 0],
+			[4655.0, 0, 0, 0, 0, 0, 0, 0, None, None, None, None, 0, 0, 0, 0, 0, 0],
+			[4655.0, 0, 0, 0, 0, 0, 0, 0, None, None, None, None, 0, 0, 0, 0, 0, 0],
+			[5100.0, 0, 0, 0, 0, 0, 0, 0, None, None, None, None, 0, 0, 0, 0, 0, 0],
+			[5100.0, 0, 0, 0, 0, 0, 0, 0, None, None, None, None, 0, 0, 0, 0, 0, 0]]
+
+for i in range(len(testList)): #for entry in testList: #modifying entry wont update testList
+	entry = testList[i]	#Makes a copy of values
+	testList[i]	= [(0 if j is None else j) for j in testList[i]]#filter out any suprises
+	print( entry )
+#for entry in testList:
+#	entry = [(0 if j is None else j) for j in entry]
+#	print( entry )
+for x in testList: print(x)"""
